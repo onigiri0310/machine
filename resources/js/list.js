9208e00eb0e4341c35a8cd0d9c408ca3d6b7f11b
onigiri0310 <mi28.sayaka@gmail.com>
2023-07-18 00:41:58 +0900

    コミットメッセージ

--- resources/js/list.js
+++
@@ -1,3 +0,0 @@
-{/* <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxy/1.6.1/scripts/jquery.ajaxy.min.js" integrity="sha512-q2iMuzh/xS/3MFfLISnA5MB0uc9oK4y/xpNZN5kHUm5HlTYXCXOGB4hrj2gWmqj/U2xFh1rNtP1kLX3uBnFfgw==" crossorigin="anonymous"></script> */}
