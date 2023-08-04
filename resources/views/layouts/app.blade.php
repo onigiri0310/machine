ccab0754023e168dd2cdf0cd4dcc137cbb263dff
onigiri0310 <mi28.sayaka@gmail.com>
2023-08-04 16:05:50 +0900

    コミットメッセージ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
         crossorigin="anonymous">
     </script>
+    <script src="{{ asset('js/product.js') }}"></script>
 
 </head>
 <body>
