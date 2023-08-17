75892c9b18556acff991e1cffc0ce491c897f519
onigiri0310 <mi28.sayaka@gmail.com>
2023-08-17 13:30:43 +0900

    賞品一覧表示の削除

--- public/js/product.js
+++ public/js/product.js
@@ -1,96 +1,6 @@
 console.log("通った");
 
 $(document).ready(function() {
-    // 商品一覧の非同期表示
-    function displayProductList() {
-
-        let csrfToken = $('#product-list').data('csrf-token');
-
-        $.ajax({
-            url: getListAjaxUrl,
-            type: "GET",
-            dataType: "json",
-            success: function(response) {
-                // 取得したデータを表示する処理
-                let productList = $('#product-list');
-                productList.empty();
-                console.log("いちらん");
-
-                // テーブルヘッダーを作成
-                let tableHeader = $('<tr><th class="sortable" data-column="id" data-sort="desc">ID</th><th class="sortable" data-column="img_path" data-sort="asc">商品画像</th><th class="sortable" data-column="product_name" data-sort="asc">商品名</th><th class="sortable" data-column="price" data-sort="asc">価格</th><th class="sortable" data-column="stock" data-sort="asc">在庫数</th><th class="sortable" data-column="company_name" data-sort="asc">メーカー名</th></tr>');
-                productList.append(tableHeader);
-
-                // 商品データをテーブルに追加
-                response.forEach(function(product) {
-                    let tableRow = $('<tr></tr>');
-                    console.log("テーブル");
-                    tableRow.append('<td>' + product.id + '</td>');
-                    tableRow.append('<td><img src="http://localhost:8888/machine/public/storage/' + product.img_path + '" class="img-fluid col-6"></td>');
-                    tableRow.append('<td>' + product.product_name + '</td>');
-                    tableRow.append('<td>' + product.price + '</td>');
-                    tableRow.append('<td>' + product.stock + '</td>');
-                    tableRow.append('<td>' + product.company.company_name + '</td>');
-
-
-                    // 詳細ボタンを追加
-                    let detailUrl = `http://localhost:8888/machine/public/detail`;
-                    let detailButton = $('<button></button>').attr({
-                        type: 'button',
-                        class: 'btn btn-primary'
-                    }).text('詳細');
-                    detailButton.click(function() {
-                        window.location.href = detailUrl + '/' + product.id;
-                    });
-                    let detailCell = $('<td></td>').append(detailButton);
-
-                    console.log('detailUrl:', detailUrl);
-
-                    tableRow.append(detailCell);
-
-
-
-                    // 削除ボタンを追加
-                    let destroyUrl = `http://localhost:8888/machine/public/products`;
-                    console.log('destroyUrl:', destroyUrl);
-                    console.log('productId:', product.id);
-
-                    let deleteForm = $('<form></form>').attr({
-                        action: destroyUrl,
-                        method: 'POST',
-                        onsubmit: "return confirm('本当に削除しますか？'); deleteProduct(event, " + product.id + ");"
-                    });
-                    deleteForm.append($('<input>').attr({
-                        type: 'hidden',
-                        name: '_method',
-                        value: 'DELETE'
-                    }));
-                    deleteForm.append($('<input>').attr({
-                        type: 'hidden',
-                        name: '_token',
-                        value: csrfToken
-                    }));
-                    deleteForm.append($('<button></button>').attr({
-                        type: 'submit',
-                        class: 'btn btn-danger'
-                    }).text('削除'));
-                    let deleteCell = $('<td></td>').append(deleteForm);
-                    tableRow.append(deleteCell);
-
-                    productList.append(tableRow);
-                });
-            },
-            error: function(xhr, status, error) {
-                // エラーハンドリングの処理
-                alert('エラーが発生しました');
-            }
-        });
-    }
-
-    // 初期表示時に商品一覧を表示
-    displayProductList();
-
-
-
 
     // 検索フォームの送信イベント
     $('#search-form').submit(function(event) {
@@ -160,13 +70,17 @@ $(document).ready(function() {
 
 
     // 商品削除ボタンのクリックイベント処理
-    $('#product-list').on('click', '.btn-danger', function(event) {
+    $('.btn-danger').click(function(event) {
         event.preventDefault();
-        let productId = $(this).data('product-id');
+        let productId = $(this).data('id');
+
+        console.log(productId);
+        console.log(destroyUrl);
 
         // 商品削除リクエスト送信
         $.ajax({
-            url: '/products/' + productId,
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            url: destroyUrl,
             type: 'DELETE',
             dataType: 'json',
             success: function(data) {
