a787e9178b823c6ba30754af9be4ebb43e56d32e
onigiri0310 <mi28.sayaka@gmail.com>
2023-09-08 15:08:08 +0900

    トランザクションの追加

--- app/Http/Controllers/API/SalesController.php
+++ app/Http/Controllers/API/SalesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Sale;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class SalesController extends Controller
 {
@@ -14,28 +15,40 @@ class SalesController extends Controller
         $productId = $request->input('product_id');
         $quantity = $request->input('quantity');
 
-        //商品の在庫数を取得
-        $product = Product::find($productId);
+        // トランザクションを開始
+        DB::beginTransaction();
 
-        if(!$product){
-            return response()->json(['message' => '商品が存在しません'],404);
+        try {
+            // 商品の在庫数を取得
+            $product = Product::find($productId);
+
+            if (!$product) {
+                return response()->json(['message' => '商品が存在しません'], 404);
+            }
+
+            // 在庫が残っているかの確認
+            if ($product->stock < $quantity) {
+                return response()->json(['message' => '在庫が不足しています'], 400);
+            }
+
+            // 購入処理
+            $sale = new Sale();
+            $sale->product_id = $productId;
+            $sale->quantity = $quantity;
+            $sale->save();
+
+            // 在庫減算処理
+            $product->stock -= $quantity;
+            $product->save();
+
+            // トランザクションをコミット
+            DB::commit();
+
+            return response()->json(['message' => '購入が完了しました'], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return response()->json(['message' => '購入処理中にエラーが発生しました'], 500);
         }
-
-        //在庫が残っているかの確認
-        if ($product->stock < $quantity){
-            return response()->json(['message' => '在庫が不足しています'],400);
-        }
-
-        //購入処理
-        $sale = new Sale();
-        $sale->product_id = $productId;
-        $sale->quantity = $quantity;
-        $sale->save();
-
-        //在庫減算処理
-        $product->stock -= $quantity;
-        $puroduct->save();
-
-        return response()->json(['message' => '購入が完了しました'],200);
     }
 }
